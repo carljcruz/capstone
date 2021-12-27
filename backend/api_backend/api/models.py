fff789f10e0464ef340566d5bbd4bc6375210c58
Carl <48541404+carljcruz@users.noreply.github.com>
2021-12-27 16:13:00 +0800

    added groups

--- backend/api_backend/api/models.py
+++ backend/api_backend/api/models.py
@@ -26,6 +26,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     date_joined = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     is_deleted = models.BooleanField(default=False)
     created_date = models.DateTimeField(default=timezone.now)
     modified_date = models.DateTimeField(default=timezone.now)
