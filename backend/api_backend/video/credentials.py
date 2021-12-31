cf88c165fede7b74431db0fa47feab9a04f82ceb
Carl <48541404+carljcruz@users.noreply.github.com>
2021-12-31 18:27:20 +0800

    added pseudocode for vid

---
+++ backend/api_backend/video/credentials.py
@@ -0,0 +1,17 @@
+# Twilio
+
+
+# Pseudo Code
+# twilio_api = 'api_key'
+# hash
+
+# if twilio_api is True
+#     then send it
+# else
+#     key does not work, try again
+
+
+# def get_room():
+#     ''' Function to get room
+#         and pass the credentials'''
+
